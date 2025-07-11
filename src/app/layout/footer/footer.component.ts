bb3c7f9bd73b1b6fe2f14e8085416b611aace5ac
mzamoun <mourad.zamoun@gmail.com>
2025-07-11 16:46:48 +0200

    set username

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2025-07-11 16:31:22"
+	dateCommit = "Last Commit : 2025-07-11 16:43:46"
   dateFooter = "";
 
   constructor() { }
