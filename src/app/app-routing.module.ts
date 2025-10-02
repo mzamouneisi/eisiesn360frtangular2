791e1daa9aedaf022c8e0b98b97eb57f1a3c6999
mzamoun <mourad.zamoun@gmail.com>
2025-10-02 16:07:56 +0200

    useHash = true

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -138,7 +138,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes , { useHash: false } )
+    RouterModule.forRoot(routes , { useHash: true } )
   ],
   exports: [RouterModule],
 })
