bac646e9f3077a2af7205aa3b39342e7f287d59a
mzamoun <mourad.zamoun@gmail.com>
2025-10-02 17:50:10 +0200

    add refresh btn for list arrondi

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2025-10-02 17:41:53"
+	dateCommit = "Last Commit : 2025-10-02 17:47:29"
   dateFooter = "";
 
   constructor() { }
