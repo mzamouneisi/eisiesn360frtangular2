f24b69f24f818ac32e30d16c2a929fb1fea81c74
mzamoun <mourad.zamoun@gmail.com>
2025-10-02 18:17:59 +0200

    add email.valid

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2025-10-02 17:54:54"
+	dateCommit = "Last Commit : 2025-10-02 18:13:21"
   dateFooter = "";
 
   constructor() { }
