e964702396482741f7c8677077fbcc975ada703c
mzamouneisi <mourad.zamoun@eisi-consulting.fr>
2025-10-21 10:10:12 +0200

    manager_peut_changer_status_cra/conge_validated

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2025-10-21 09:37:57"
+	dateCommit = "Last Commit : 2025-10-21 10:06:42"
   dateFooter = "";
 
   constructor() { }
