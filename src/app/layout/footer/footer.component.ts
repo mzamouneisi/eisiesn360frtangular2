fdb30357d5a98ddd87465fae228a7331ce215957
mzamouneisi <mourad.zamoun@eisi-consulting.fr>
2025-11-10 11:21:32 +0100

    add_index.html_sous_connetion

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2025-11-10 11:00:57"
+	dateCommit = "Last Commit : 2025-11-10 11:18:19"
   dateFooter = "";
 
   constructor() { }
