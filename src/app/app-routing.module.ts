b838bbd254ea41d67e4a4d4d7269a966a3cef8cd
mzamouneisi <mourad.zamoun@eisi-consulting.fr>
2025-11-10 16:40:30 +0100

    reorg_routing_useHash:_true

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -137,7 +137,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes, { useHash: false })
+    RouterModule.forRoot(routes, { useHash: true  })
   ],
   exports: [RouterModule],
 })
