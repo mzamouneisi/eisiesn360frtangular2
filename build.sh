6675fb540eee120cf4b4df92f6f3a40280ed7426
mzamouneisi <mourad.zamoun@eisi-consulting.fr>
2025-11-13 15:01:48 +0100

    reorg_routing_13

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 # ng build --output-path docs --base-href /eisiesn360frtangular/
 # ng build --configuration production --output-path docs --base-href /eisiesn360frtangular2/
 # ng build --prod --base-href /eisiesn360frtangular2/
-ng build --output-path docs --base-href ./
+# ng build --output-path docs --base-href ./
+ng build --output-path docs --base-href .
