11761a5c4851a7ef02dc838d4ec170b0ea5f98c4
mzamouneisi <mourad.zamoun@eisi-consulting.fr>
2026-01-23 11:06:53 +0100

    process_cra_OK

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2026-01-22 17:33:50"
+	dateCommit = "Last Commit : 2026-01-23 11:01:26"
   dateFooter = "";
 
   constructor() { }
