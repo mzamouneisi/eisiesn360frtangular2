eb4b1d2aa6b8097655505ec1cab73206398d8ab2
mzamouneisi <mourad.zamoun@eisi-consulting.fr>
2026-02-05 17:07:15 +0100

    debug_activity_tjm

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   today = new Date();
-	dateCommit = "Last Commit : 2026-02-05 16:43:45"
+	dateCommit = "Last Commit : 2026-02-05 17:03:12"
   dateFooter = "";
 
   constructor() { }
